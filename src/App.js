352e7fcfc80b5d15966549e328e12085988435ce
Aamish Hussain <aamishhussain23@gmail.com>
2023-09-26 20:02:41 +0530

    learnt about props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Greeting from './Greeting';
 
 function App() {
   return (
     <div className="App">
-      
+      <Greeting name={'rahul'}></Greeting>
+      <Greeting name={'ajay'}></Greeting>
+      <Greeting name={'vijay'}></Greeting>
+      <Greeting name={'neeraj'}></Greeting>
     </div>
   );
 }
