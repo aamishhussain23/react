c722fb827d8b6a144321f9e2b630482a2db8dece
Aamish Hussain <aamishhussain23@gmail.com>
2023-10-01 22:47:03 +0530

    learnt useRef hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { useRef } from 'react';
 import './App.css';
 
 function App() {
+
+  let inputRef = useRef(null)
+  function handleBtn(){
+      inputRef.current.value = "hello world"
+      inputRef.current.focus()
+      inputRef.current.style.color = 'red'
+      // inputRef.current.style.display = 'none'
+  }
+
   return (
     <div className="App">
-      
+      <input type="text" ref={inputRef} />
+      <button onClick={handleBtn}>Handle input</button>
     </div>
   );
 }
